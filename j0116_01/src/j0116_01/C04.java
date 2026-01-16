807f30a001ecba760c308dde4007684421ac0b71
rladudals1234 <goadiddl@naver.com>
2026-01-16 14:25:24 +0900

    test23

--- j0116_01/src/j0116_01/C04.java
+++ j0116_01/src/j0116_01/C04.java
@@ -8,13 +8,13 @@ public class C04 {
 		System.out.println(1.2345678);
 		System.out.println(10/(double) 3);
 		System.out.println(10/(float) 3);
-		
+
 		//printf -> %
 		// d-정수, f-실수
 		System.out.printf("%.2f\n",10/(double)3);
 		System.out.printf("%010d,%.2f",123,10/(double)3);
 		System.out.printf("\n%x",151);
-		
+
 		//출력
 		System.out.println("\ntest");
 		//입력 - System : console에서 입력을 받겠다.
@@ -22,7 +22,7 @@ public class C04 {
 		System.out.printf("숫자를 입력하세요.>> ");
 		int num = scan.nextInt();	//정수형 타입 값을 받음.
 		System.out.println("입력숫자 : "+num);
-		//test1
+		//test12
 	}
 
 }
