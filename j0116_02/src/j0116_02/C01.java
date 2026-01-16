7f49e82a31edd95c4568a26569f01216d2130153
rladudals1234 <goadiddl@naver.com>
2026-01-16 18:24:19 +0900

    test

---
+++ j0116_02/src/j0116_02/C01.java
@@ -0,0 +1,10 @@
+package j0116_02;
+
+public class C01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ j0116_03/src/j0116_03/C01.java
@@ -0,0 +1,10 @@
+package j0116_03;
+
+public class C01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//test
+	}
+
+}
